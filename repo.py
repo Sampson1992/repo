4ad7526abfa876df30c2962e58a105154cdcccf2
Sampson Boamah <boamah.sampson34@gmail.com>
2023-05-11 08:58:45 +0000

    first commit

---
+++ repo.py
@@ -0,0 +1,46 @@
+from re import I
+
+
+A = 8*5
+print (A)
+# class work
+# conditionals 
+# if statement
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+# elif statement
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+    # else statement
+    a = 200
+    b = 33
+    if b > a:
+        print("b is greater than a")
+    elif a == b:
+        print("a and be are equal")
+    else:
+        print("a is geater b") 
+
+    
+
+        # loops
+        #the while loop
+        #print i as long as i is less than 6
+        i = 1
+        while i < 6:
+            print(i)
+
+            # exit the loop when i is 3
+            i = 1
+            while i < 6:
+                print(i)  
+i += 1
+        
